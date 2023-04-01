ffcdfb7909bfb8dc443332b30eb6d820ec7f0b8d
Chi Nguyen Dac <dacchi6761@gmail.com>
2023-04-01 17:13:44 +0700

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,23 +3,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { DB_CONFIG } from './config';
-import { User } from './entities/user.entity';
 
 @Module({
-  imports: [
-    AuthModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: DB_CONFIG.HOST,
-      port: DB_CONFIG.PORT,
-      username: DB_CONFIG.USER_NAME,
-      password: DB_CONFIG.PASSWORD,
-      database: DB_CONFIG.DB,
-      entities: [User],
-      synchronize: false,
-    }),
-  ],
+  imports: [AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from 'src/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [],
   controllers: [AuthController],
   providers: [AuthService],
 })
