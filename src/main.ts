618ba5a9e4f666c5fe196c1029f57379f467dafc
coliamai <dacchi6761@gmail.com>
2023-04-02 22:20:42 +0700

    response interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './shared/filters/http-exception.filter';
+import { ResponseInterceptor } from './shared/interceptors/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
   app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalInterceptors(new ResponseInterceptor());
 
   await app.listen(3000);
 }
---
+++ src/shared/interceptors/response.interceptor.ts
@@ -0,0 +1,32 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  HttpStatus,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable, map } from 'rxjs';
+
+export interface ResponseType<T> {
+  statusCode: number;
+  errorCode: number;
+  message: string;
+  data: T;
+}
+
+@Injectable()
+export class ResponseInterceptor<T> implements NestInterceptor<T, ResponseType<T>> {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<ResponseType<T>> {
+    let statusCode = context.switchToHttp().getResponse().statusCode;
+    let errorCode = [HttpStatus.OK, HttpStatus.CREATED].includes(statusCode) ? 0 : 1;
+
+    return next.handle().pipe(
+      map((data) => ({
+        statusCode: statusCode,
+        errorCode,
+        message: data.message || '',
+        data: data,
+      }))
+    );
+  }
+}
