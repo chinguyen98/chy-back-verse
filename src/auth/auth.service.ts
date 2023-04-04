0fd4dd0da27231da8de0fe75f894a13902cf4544
coliamai <dacchi6761@gmail.com>
2023-04-05 01:05:09 +0700

    DataModule global

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,11 @@
 import { Injectable, NotAcceptableException } from '@nestjs/common';
 import { isValidDate } from 'src/shared/libs/daytime';
 import { RegisterCredentialsDto, RegisterResponseDto } from './dto/auth.dto';
+import { IDataServices } from 'src/shared/core/data-services.abstract';
 
 @Injectable()
 export class AuthService {
-  // constructor(@InjectModel(User.modelName) private readonly _userModel: ModelType<User>) {}
+  constructor(private dataServices: IDataServices) {}
 
   async signUp(registerDto: RegisterCredentialsDto): Promise<RegisterResponseDto> {
     const { date_of_birth } = registerDto;
--- src/data/data.module.ts
+++ src/data/data.module.ts
@@ -1,6 +1,7 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { MongoDataModule } from './mongo/mongo-data.module';
 
+@Global()
 @Module({
   imports: [MongoDataModule],
   exports: [MongoDataModule],
