c9e528303a9da427568e4395f9b7fad427a09423
coliamai <dacchi6761@gmail.com>
2023-05-26 23:28:04 +0700

    add cors

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,23 @@ import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './shared/filters/http-exception.filter';
 import { ResponseInterceptor } from './shared/interceptors/response.interceptor';
 
+const corsWhiteList = [];
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalInterceptors(new ResponseInterceptor());
+  app.enableCors({
+    origin: (origin, callback) => {
+      if (!origin || corsWhiteList.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+  });
 
   await app.listen(3000);
 }
