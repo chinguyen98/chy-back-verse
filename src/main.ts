b36a02da59dbbc3cdb785d748f56761afa7702c6
coliamai <dacchi6761@gmail.com>
2023-05-28 10:59:14 +0700

    .

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './shared/filters/http-exception.filter';
 import { ResponseInterceptor } from './shared/interceptors/response.interceptor';
 
-const corsWhiteList = ['http://localhost:3000', 'https://coliamai.uk'];
+const corsWhiteList = ['http://localhost:3000', 'https://coliamai.uk', 'http://127.0.0.1:3000'];
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
