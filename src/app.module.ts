039541b5e56ae2efb5ccadd6d885f21edcd05799
coliamai <dacchi6761@gmail.com>
2023-06-12 23:12:11 +0700

    .

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,15 @@
 import { CacheModule } from '@nestjs/cache-manager';
-import type { RedisClientOptions } from 'redis';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { redisStore } from 'cache-manager-redis-yet';
+import type { RedisClientOptions } from 'redis';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { DataModule } from './data/data.module';
 import Config from './shared/configs';
 import { VerificationModule } from './verification/verification.module';
-import { redisStore } from 'cache-manager-redis-yet';
+import { ServersModule } from './servers/servers.module';
 
 @Module({
   imports: [
@@ -23,6 +24,7 @@ import { redisStore } from 'cache-manager-redis-yet';
     DataModule,
     AuthModule,
     VerificationModule,
+    ServersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
