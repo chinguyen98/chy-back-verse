e9b70137e4b2452d12ba31387d38c8f0b16251ee
coliamai <dacchi6761@gmail.com>
2023-12-16 22:44:29 +0700

    .

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,38 +1,33 @@
 import { RedisModule } from '@nestjs-modules/ioredis';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { BoardsModule } from './boards/boards.module';
 import { DataModule } from './data/data.module';
 import { RefreshTokenModule } from './refresh-token/refresh-token.module';
 import Config from './shared/configs';
 import { VerificationModule } from './verification/verification.module';
-import { PostsModule } from './posts/posts.module';
-import { TesterModule } from './tester/tester.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, load: [() => Config] }),
-    // RedisModule.forRoot({
-    //   config: {
-    //     username: Config.redis.username,
-    //     password: Config.redis.password,
-    //     host: Config.redis.host,
-    //   },
-    // }),
+    RedisModule.forRoot({
+      config: {
+        username: Config.redis.username,
+        password: Config.redis.password,
+        host: Config.redis.host,
+      },
+    }),
     // GraphQLModule.forRoot<ApolloDriverConfig>({
     //   driver: ApolloDriver,
     // }),
     DataModule,
-    TesterModule,
-    // AuthModule,
-    // VerificationModule,
+    // TesterModule,
+    AuthModule,
+    VerificationModule,
     // BoardsModule,
-    // RefreshTokenModule,
+    RefreshTokenModule,
     // PostsModule,
   ],
   controllers: [AppController],
